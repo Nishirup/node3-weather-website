9d127f742f8521dd3c8b9d17e01f8508e7480dbf
U-AzureAD\NishitaTiwary <nishirup2015@gmail.com>
2020-05-06 12:54:14 +0530

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,3 @@
-console.log('Client side javascript file is loaded!!')
 
 //fetch () is used to fetch values from an api 
 //the syntax used below means - fetch the value from the url and then get the response - once the reponse is received - parse it into a js object and dump it to the console or render it on the browser page.
